1e63346d1dd6299e6bf430edb36e0240f9e4355b
Danil Platonov <platonov.danil.ivanovich@gmail.com>
2026-07-29 08:01:25 -0700

    trips

---
+++ csv2pl.py
@@ -0,0 +1,44 @@
+#!/bin/env python3
+
+# Script to convert any csv file into a Prolog database
+
+import pandas as pd
+from sys import argv
+import re
+
+def snake2camel(s):
+    ts = s.split("_")
+    f = ts[0]
+    r = [x.capitalize() for x in ts[1:]]
+    return "".join([f.lower()] + r)
+
+def encode(v):
+    if type(v) is str:
+        # check if it's a timestamp
+        ts = re.findall(r"^\s*(\d?\d):(\d\d):(\d\d)\s*$", " 5:04:00")
+        if ts:
+            (h,m,s) = ts[0]
+            return str(int(s) + 60*int(m) + 60*60*int(h))
+
+        return f'"{v}"'
+    if type(v) is int or float:
+        return str(v)
+
+    raise Error(f"Unsupported type: {type(v)} for {v}")
+
+def main():
+    if len(argv) < 3:
+        print(f"Usage: {argv[0]} [csv_in] [prolog_out]")
+        return
+
+    df = pd.read_csv(argv[1])
+
+    with open(argv[2], "w") as f:
+        for _, row in df.iterrows():
+            d = dict(row)
+            keys = list(snake2camel(x) for x in d.keys())
+            pred = "_".join(keys)
+            vals = ', '.join(encode(v) for v in d.values())
+            f.write(f"{pred}({vals}).\n")
+
+main()
---
+++ trips.pl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7c0843797540f0f87de8eaed5a4fffe43465fffeddbd5a1311a87558a0cc1ea1
+size 11508085
